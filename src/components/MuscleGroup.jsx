915145348e2b5cd363baec7e65defcee7ea1d5f3
HeatherRouke <heather214@gmail.com>
2024-02-12 11:35:21 -0500

    muscle group style

--- src/components/MuscleGroup.jsx
+++ src/components/MuscleGroup.jsx
@@ -4,15 +4,17 @@ import { Link } from "react-router-dom"
 const MuscleGroup = ({muscleGroups}) => {
     return (
         <div>
-            <h1>Muscle Map</h1>
+            <h1 className="group-title">Muscle Map</h1>
+            <div className="group-container">
             {muscleGroups.map(muscle => (
-                <div key={muscle.id}>
+                <div key={muscle.id} className="muscle-box">
                     <Link to={`/movements/${muscle._id}`}>
-                    <h4>{muscle.name}</h4>
+                    <h4 className="muscle-name">{muscle.name}</h4>
                     <img src={muscle.image} alt={muscle.name} />
                     </Link>
                 </div>
             ))}
+            </div>
         </div>
     );
 }
